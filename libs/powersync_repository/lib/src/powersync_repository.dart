550808ab52c93099ee82202afd1e49e299239a3c
Krzysztof Domanski <krzysiekdomanski@hotmail.com>
2025-02-27 21:02:34 +0000

    Added powersync repo and update line length format for github job

---
+++ libs/powersync_repository/lib/src/powersync_repository.dart
@@ -0,0 +1,7 @@
+/// {@template powersync_repository}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class PowersyncRepository {
+  /// {@macro powersync_repository}
+  const PowersyncRepository();
+}
